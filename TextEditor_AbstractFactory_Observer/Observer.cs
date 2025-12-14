d1affb02b9333d93b0508e504f15a5c5194d783d
Quasar <krivokorytovsasha@gmail.com>
2025-12-14 13:50:50 +0200

    Add Observer base (Subject + Interface)

---
+++ TextEditor_AbstractFactory_Observer/Observer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace TextEditor_AbstractFactory_Observer
+{
+    public interface ITextObserver
+    {
+        void Update(string oldText, string newText);
+    }
+
+    public class TextEditor
+    {
+        private string text = "";
+        private List<ITextObserver> observers = new List<ITextObserver>();
+
+        public void Attach(ITextObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void SetText(string newText)
+        {
+            string old = text;
+            text = newText;
+
+            foreach (var o in observers)
+            {
+                o.Update(old, newText);
+            }
+        }
+    }
+}
