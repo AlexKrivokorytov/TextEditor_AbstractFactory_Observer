3fe6aece257c46ec5fffae2296cf01f9feeb673a
Quasar <krivokorytovsasha@gmail.com>
2025-12-14 13:34:03 +0200

    Add project files.

---
+++ TextEditor_AbstractFactory_Observer/FileIO.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+public abstract class FileLoader
+{
+    public abstract string Load(string path);
+}
+
+public abstract class FileSaver
+{
+    protected string Text;
+
+    protected FileSaver(string text)
+    {
+        Text = text;
+    }
+
+    public abstract void Save(string path);
+}
